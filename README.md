c41935c6d45d211958c211c776b81a145e56ebff
Nikita <jrringler13759@gmail.com>
2020-05-07 18:26:24 -0400

    checking links in readme

--- README.md
+++ README.md
@@ -9,11 +9,17 @@
   
   ## Table of Contents
   *[Installation](#installation)
+
   *[Usage](#usage)
+
   *[License](#license)
+
   *[Contributing](#contributing)
+
   *[Tests](#tests)
+
   *[Questions](#questions)
+  
   
   ## Installation
   To install necessary dependencies, run the following command:
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,11 +10,17 @@ function generateMarkdown (answers) {
   
   ## Table of Contents
   *[Installation](#installation)
+
   *[Usage](#usage)
+
   *[License](#license)
+
   *[Contributing](#contributing)
+
   *[Tests](#tests)
+
   *[Questions](#questions)
+  
   
   ## Installation
   To install necessary dependencies, run the following command:
